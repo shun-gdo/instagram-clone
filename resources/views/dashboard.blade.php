e85fab287da1c04b7c4f1594bac36893769d6098
shunta <shunta.gdo@gmail.com>
2024-07-01 02:25:30 +0000

    fix index views

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,18 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('layouts.app')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+@section('content')
+    // ＊投稿できるようになり次第追加:投稿があればforeachで表示
+    @if(true)
+        <p>ここに投稿一覧が表示されます</p>
+    @else
+        <div class="prose hero bg-base-200 mx-auto max-w-full rounded">
+            <div class="hero-content text-center my-10">
+                <div class="max-w-md mb-10">
+                    <h2>Welcome to the Instagram Clone</h2>
+                    {{-- ユーザー登録ページへのリンク --}}
+                    <a class="btn btn-primary btn-lg normal-case" href="{{ route('register') }}">Sign up now!</a>
                 </div>
             </div>
         </div>
-    </div>
-</x-app-layout>
+    @endif
+@endsection
