b836dc6b7d6268d3a7773168ec70b66b41235f66
shunta <shunta.gdo@gmail.com>
2024-07-01 01:44:15 +0000

    add tailwind and daisy ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import typography  from '@tailwindcss/typography';  // 追記
+import daisyui from 'daisyui';                      // 追記
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -18,4 +20,5 @@ export default {
     },
 
     plugins: [forms],
+    plugins: [typography, daisyui],     // 追記
 };
