4d37910f2d39bb7d672a7c33ac2f5aa1f9dbf770
shunta <shunta.gdo@gmail.com>
2024-07-03 07:06:51 +0000

    fix post card style

--- resources/views/posts/posts.blade.php
+++ resources/views/posts/posts.blade.php
@@ -1,27 +1,10 @@
 <div class="mt-4">
     @if (isset($posts))
-        <ul class="list-none">
+        <!--<ul class="list-none">-->
             @foreach ($posts as $post)
-                <li class="flex items-start justify-center gap-x-2 mb-4">
-                    
-                        <div class="card bg-base-100 w-3/5 shadow-xl">
-                            
-                            <figure>
-                                <img src="{{ asset('/storage/images/'.$post->img_name) }}"
-                                class="object-cover aspect-square w-full h-" />
-                            </figure>
-                            
-                            <div class="card-body">
-                                <p>{{ $user->name }}<span>( {{ $user->email }} )</span></p>
-                                <p>{{ $post->caption }}</p>
-                                <div class="card-actions justify-end">
-                                    <button class="btn btn-secondary">Favorite</button>
-                                </div>
-                            </div>
-                        </div>
-                </li>
+                @include('posts.post_card')
             @endforeach
-        </ul>
+        <!--</ul>-->
 
     @endif
 </div>
